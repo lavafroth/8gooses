58ef1e1000180d6939f3c2440113a49c9cecb769
lavafroth <lavafroth@protonmail.com>
2022-08-30 11:57:00 +0530

    Removed some helper functions, refactored others.

--- main.go
+++ main.go
@@ -14,10 +14,11 @@ import (
 )
 
 const base string = "https://comics.8muses.com"
+
 var re *regexp.Regexp
 
 func DownloadArtist(tags []string) error {
-	links, err := Links(tags)
+	links, err := Links(tags, "a", "href")
 	if err != nil {
 		return err
 	}
@@ -30,7 +31,7 @@ func DownloadArtist(tags []string) error {
 }
 
 func DownloadAlbum(tags []string) error {
-	links, err := Links(tags)
+	links, err := Links(tags, "a", "href")
 	if err != nil {
 		return err
 	}
@@ -43,38 +44,35 @@ func DownloadAlbum(tags []string) error {
 }
 
 func DownloadEpisode(tags []string) error {
-	pages, directory, err := Enumerate(tags)
-	if err != nil {
-		return err
-	}
-	images, err := Images(pages)
-	if err != nil {
-		return err
-	}
+	var directory string
+	directory = filepath.Join(tags[len(tags)-3:]...)
 	episode := tags[len(tags)-1]
 	log.Printf("Downloading episode %s to %s", episode, directory)
-	DownloadAll(images, directory)
-	return nil
-}
+	os.MkdirAll(directory, 0o700)
+	links, err := Links(tags, ".image img", "data-src")
+	if err != nil {
+		return fmt.Errorf("while parsing episode metadata: %q", err)
+	}
+	for i, link := range links {
+		fragments := strings.Split(strings.Trim(link, "/"), "/")
+		if len(fragments) < 3 {
+			return fmt.Errorf("while parsing image episode metadata: expected image location to have at least 3 fragments: found %d", len(fragments))
+		}
+		fragments = fragments[len(fragments)-3:]
+		fragments[1] = "fl"
+		source, err := url.JoinPath(base, fragments...)
+		if err != nil {
+			return fmt.Errorf("while parsing episode metadata: %q", err)
+		}
+		if err := Download(filepath.Join(
+			directory,
+			fmt.Sprintf("%d%s", i, filepath.Ext(source)),
+		), source); err != nil {
+			return fmt.Errorf("while parsing episode metadata: %q", err)
+		}
 
-func DocumentAt(tags []string) (*goquery.Document, error) {
-	uri, err := AbsoluteURL(tags)
-	if err != nil {
-		return nil, err
 	}
-	res, err := http.Get(uri)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("status code error: %d %s for url: %s", res.StatusCode, res.Status, uri)
-	}
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	return doc, nil
+	return nil
 }
 
 func Download(destination string, source string) error {
@@ -95,96 +93,54 @@ func Download(destination string, source string) error {
 	return nil
 }
 
-func Fragments(path string) []string {
-	return strings.Split(strings.Trim(path, "/"), "/")
-}
-
-func Images(links []string) ([]string, error) {
-	var images []string
-	for _, link := range links {
-		fragments := Fragments(link)
-		if len(fragments) < 3 {
-			return images, fmt.Errorf("while parsing image episode metadata: expected image location to have at least 3 fragments: found %d", len(fragments))
-		}
-		fragments = fragments[len(fragments)-3:]
-		fragments[1] = "fl"
-		source, err := url.JoinPath(base, fragments...)
-		if err != nil {
-			return images, fmt.Errorf("while parsing episode metadata: %q", err)
-		}
-		images = append(images, source)
-	}
-	return images, nil
-}
-
-func DownloadAll(images []string, directory string) error {
-	for i, source := range images {
-		if err := Download(filepath.Join(
-			directory,
-			fmt.Sprintf("%d%s", i, filepath.Ext(source)),
-		), source); err != nil {
-			return fmt.Errorf("while parsing episode metadata: %q", err)
-		}
-	}
-	return nil
-}
-
-func Enumerate(tags []string) ([]string, string, error) {
-	var pages []string
-	var directory string
-	doc, err := DocumentAt(tags)
-	if err != nil {
-		return pages, directory, fmt.Errorf("while parsing episode metadata: %q", err)
-	}
-	directory = filepath.Join(tags[len(tags)-3:]...)
-	os.MkdirAll(directory, 0o700)
-	doc.
-		FindMatcher(goquery.Single(".gallery")).
-		Find(".image img").
-		Each(func(i int, s *goquery.Selection) {
-			if location, ok := s.Attr("data-src"); ok {
-				pages = append(pages, location)
-			}
-		})
-	return pages, directory, nil
-}
-
-func Links(tags []string) ([]string, error) {
+func Links(tags []string, selector string, attribute string) ([]string, error) {
 	var links []string
-	doc, err := DocumentAt(tags)
+
+	uri, err := AbsoluteURL(tags)
 	if err != nil {
-		return links, err
+		return nil, err
 	}
+	res, err := http.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("status code error: %d %s for url: %s", res.StatusCode, res.Status, uri)
+	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
 	doc.
 		FindMatcher(goquery.Single(".gallery")).
-		Find("a").
+		Find(selector).
 		Each(func(i int, s *goquery.Selection) {
-			if location, ok := s.Attr("href"); ok {
+			if location, ok := s.Attr(attribute); ok {
 				links = append(links, location)
 			}
 		})
 	return links, nil
 }
 
-// Tags strips a URL or partial URL to the artist, album and optionally an episode
-func Tags(path string) []string {
-	tags := []string{}
-
+// Tags filters the artist, album and optionally the episode from a complete or partial URL
+func Tags(path string) (tags []string) {
 	groups := re.SubexpNames()
 	for id, value := range re.FindAllStringSubmatch(path, -1)[0] {
 		switch groups[id] {
 		case "artist", "album", "episode":
 			if value != "" {
-			tags = append(tags, value)
+				tags = append(tags, value)
 			}
 		}
 	}
-	return tags
+	return
 }
 
-// AbsoluteURL finds the tags in a URI and reconstructs the full URL
+// AbsoluteURL uses the tags for a resource and reconstructs the full URL
 func AbsoluteURL(tags []string) (string, error) {
-	return url.JoinPath("https://comics.8muses.com/comics/album", tags...)
+	return url.JoinPath(base, append([]string{"comics", "album"}, tags...)...)
 }
 
 func main() {
@@ -196,18 +152,17 @@ func main() {
 	re = regexp.MustCompile("(https://)?(comics\\.)?(8muses\\.)?(com/)?(comics/)?((picture|album)/)?(?P<artist>[A-Za-z0-9\\-]+/?)(?P<album>[A-Za-z0-9\\-]+/?)?(?P<episode>[A-Za-z0-9\\-]+/?)?")
 
 	tags := Tags(os.Args[1])
-	var action func(tags []string) error
+
+	// Default to downloading a single episode
+	action := DownloadEpisode
 	switch len(tags) {
 	case 1:
+		// Download all episodes by an artist
 		action = DownloadArtist
 	case 2:
 		// Download all episodes in the album
 		action = DownloadAlbum
-	default:
-		// Download one episode
-		action = DownloadEpisode
 	}
-
 	if err := action(tags); err != nil {
 		log.Fatalln(err)
 	}
